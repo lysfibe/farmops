9f5fa92b2c1f2b317652f682dd1c334c1b848214
Tom Hewett <up665064@myport.ac.uk>
2017-04-29 16:07:09 +0100

    d3 & topojson for counties in the UK

--- routes.js
+++ routes.js
@@ -6,10 +6,10 @@ const api = new Router({ prefix: '/api' })
 const controller = (name, method) => require(`./controllers/${name}`)[method]
 
 web.get('/', async (ctx) => {
-	await ctx.render('test')
+	await ctx.render('index')
 })
 
-api.get('/', controller('api/example', 'index'))
+// api.get('/', controller('api/example', 'index'))
 
 exports.web = web
 exports.api = api
