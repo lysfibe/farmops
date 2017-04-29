15cd3ebd3b8c9465df28e5bc8a59f138d6479975
Tom Hewett <up665064@myport.ac.uk>
2017-04-29 20:03:56 +0100

    structured app.js file and set up init functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,26 @@
-function updateStatsPane(county) {
-    $( '.stats__county-name' ).text(county);
-}
+(function() {
+    var loader,
+        foodops;
 
-$( document ).ready(function() {
-    console.log('did stuff');
-    $( '.unit' ).click(function() {
-        console.log('did more stuff');
-        const county = $( this ).children('title').text();
-        updateStatsPane(county);
-    });
-});
+    function updateStatsPane(county) {
+        $( '.stats__county-name' ).text(county);
+    }
+
+    function initCountyListeners() {
+        if ($( '.unit' ).length > 0) {
+            $( '.unit' ).click(function() {
+                var county = $( this ).children('title').text();
+                updateStatsPane(county);
+            });
+            clearInterval(loader);
+        }
+    }
+
+    function init() {
+        loader = setInterval(initCountyListeners, 500);
+
+        // use fetch to get loads of data and chuck it into the foodops object
+    }
+
+    init();
+})();
