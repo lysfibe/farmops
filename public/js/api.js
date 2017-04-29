9e7ba2dd11e7c64cc3cd534aabf1c5e1300ada78
Louis Capitanchik <contact@louiscap.co>
2017-04-30 00:16:04 +0100

    Being lazy with query params in the least lazy way

--- public/js/api.js
+++ public/js/api.js
@@ -1,17 +1,32 @@
 window.api = window.api || {}
-
-/*
- * {
- *   name: {
- *		 population: 1000,
- *		 deficit: 5000,
- *		 containers: 40,
- *		 farmland: 1230,
- *   }
- * }
- */
-api.authority = function getAuthorities(name) {
-	name = name || ''
-	return fetch('/api/authorities/' + name)
-		.then(r => r.json())
-}
+(function() {
+	const qs = obj => {
+		let str = '?'
+		for (const key in obj) {
+			if (obj.hasOwnProperty(key)) {
+				const value = obj[key]
+				if (Array.isArray(value)) {
+					str += value.map(val => `${encodeURIComponent(key)}[]=${encodeURIComponent(val)}`).join('&')
+				} else {
+					str += encodeURIComponent(key) + '=' + encodeURIComponent(value)
+				}
+			}
+		}
+		return str
+	}
+	/*
+	* {
+	*   name: {
+	*		 population: 1000,
+	*		 deficit: 5000,
+	*		 containers: 40,
+	*		 farmland: 1230,
+	*   }
+	* }
+	*/
+	api.authority = function getAuthorities(name, params) {
+		name = name || ''
+		return fetch('/api/authorities/' + name + qs(params))
+			.then(r => r.json())
+	}
+}())
