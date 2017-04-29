d67bff31b709291f5d4a25629b6a4303a0099d36
Louis Capitanchik <contact@louiscap.co>
2017-04-30 00:10:05 +0100

    Pluck params

--- controllers/api/authorities.js
+++ controllers/api/authorities.js
@@ -18,6 +18,8 @@ const defaults = {
 	provision: 0.8,
 }
 
+const pluck = (obj, props) => props.map(prop => [prop, obj[prop]]).reduce((acc, [key, value]) => Object.assign(acc, { [key]: value }), {})
+
 function serialise({ population, farmland }, params = {}) {
 	const parameters = Object.assign({}, defaults, params)
 	const required = parameters.consumption * population
@@ -38,7 +40,7 @@ const data = require('../../data/counties.json')
 /**
  * This function will eventually be provided by Dave's data stuff
  */
-async function getData(name = null) {
+async function getData(name = null, opts = {}) {
 	if (name) {
 		const datum = data[name]
 		if (datum == null) return null
@@ -51,9 +53,9 @@ async function getData(name = null) {
 }
 
 exports.list = async ctx => {
-	ctx.body = await getData(ctx.query.name)
+	ctx.body = await getData(ctx.query.name, pluck(ctx.query, ['consumption', 'provision']))
 }
 
 exports.read = async ctx => {
-	ctx.body = await getData(ctx.params.name)
+	ctx.body = await getData(ctx.params.name, pluck(ctx.query, ['consumption', 'provision']))
 }
